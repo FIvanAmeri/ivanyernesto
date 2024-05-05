d249dc3a0c09ea93ede68d222a1a220fdc315b44
FIvanAmeri <dark.ranked@gmail.com>
2024-05-05 15:17:32 -0300

    Prueba1

---
+++ 16.js
@@ -0,0 +1,15 @@
+function esImpar(num) {
+  // La función recibe un argumento llamado num.
+  // Retorna true si el argumento "num" es impar.
+  // De lo contrario, retorna false.
+  // Por ejemplo: 
+  // 15 ---> true
+  // 14 ---> false
+  // Tu código:
+  if (num % 2 !== 0) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = esImpar;
