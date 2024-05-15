7c9ef75929dfee7f2fd033b3ddf9305f767131cb
FIvanAmeri <dark.ranked@gmail.com>
2024-05-15 11:58:06 -0300

    Prueba1

--- 08.js
+++ 08.js
@@ -7,7 +7,7 @@ function esNumeroEntero(numero) {
   // -1212 ---> true 
   // 121.212 ---> false 
   // Tu código:
-   return Number.isInteger(numero);
+  return Number.isInteger(numero);
   }
   
 
